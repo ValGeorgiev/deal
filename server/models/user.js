49c851ddd6b73f9615708d71371223fcf60b1d47
ValentinAlexandrovGeorgiev <valentin.georgiev95@abv.bg>
2017-07-14 21:01:22 +0300

    add bcrypt module add user schema

---
+++ server/models/user.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+let UserSchema = new Schema({
+  firstName: {
+    type: String
+  },
+  lastName: {
+    type: String
+  },
+  email: {
+    type: String
+    required: true,
+    trim: true,
+    match: /\b[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}\b/,
+    index: {
+      unique: true
+    }
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    enum: ['user', 'agency', 'admin'],
+    default: 'user'
+  }
+})
